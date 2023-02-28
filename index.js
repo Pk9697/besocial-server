dd4c1c5ec6eef86bc7b85b6dcc672bc201edac63
Prashant Kumar <prashant.aman9@gmail.com>
2023-02-28 17:08:32 +0530

    setup mongoose config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./routes/index.js";
+import db from "./config/mongoose.js";
 
 const port = 3001;
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 /* MIDDLEWARES */
 //use express router
 app.use("/", router);
+
+
 
 /* RUN SERVER */
 app.listen(port, function (err) {
